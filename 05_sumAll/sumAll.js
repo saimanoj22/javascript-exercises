f82cf79a086938f6aa6e0a9c371a41df3f2142ee
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-25 01:10:04 +0530

    Add 05 Solution (All 6 tests passed)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b))return "ERROR";
+    else if(a < 0 || b < 0)return "ERROR";
+    else{
+        let sum = 0;
+        for(let i = Math.min(a ,b); i <= Math.max(a, b); i++){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
