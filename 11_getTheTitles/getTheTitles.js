70d62113a0a8e29f1a3345fd0a59835d12013507
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-25 04:35:04 +0530

    Add 11 Solution (All 1 test passed)

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    let array = [];
+    for(let i = 0; i < list.length; i++){
+        array.push(list[i].title);
+    }
+    return array;
 };
 
 // Do not edit below this line
