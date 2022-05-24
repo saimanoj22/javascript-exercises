d227de8255385fcd9cd450fd27082d05f3b001c6
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-25 04:07:59 +0530

    Add 09 Solution (All 6 tests passed)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "");
+    revString = string.split('').reverse().join('');
+    return string === revString;
 };
 
 // Do not edit below this line
