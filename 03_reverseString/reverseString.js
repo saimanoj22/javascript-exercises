52e049d35ed79ae373651312ccc546f3471543bb
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-24 20:47:39 +0530

    Update with optimal code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,14 @@
 const reverseString = function(string) {
-    let strArray = string.split('')
-    let ansArray = [];
-    let start = strArray.length - 1;
-    for(let i = start; i >= 0; i--){
-        ansArray[start - i] = strArray[i];
-    }
-    let ans = ansArray.join('');
-    return ans;
+    // let strArray = string.split('')
+    // let ansArray = [];
+    // let start = strArray.length - 1;
+    // for(let i = start; i >= 0; i--){
+    //     ansArray[start - i] = strArray[i];
+    // }
+    // let ans = ansArray.join('');
+    // return ans;
+    
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
