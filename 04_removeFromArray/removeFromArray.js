af97a55111f520946942f32ceafb412c36113cbd
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-24 20:44:32 +0530

    Add 04 Solution (All 7 tests passed)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let ansArray = [];
+    let present = true;
+    for(let i = 0; i < array.length; i++){
+        present = true;
+        for(let j = 0; j < args.length; j++){
+            if(array[i] === args[j]){
+                present = false;
+            }
+        }
+        if(present === true){
+            ansArray.push(array[i]);
+        }
+    }
+    return ansArray;
 };
 
 // Do not edit below this line
