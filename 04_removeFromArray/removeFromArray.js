da1696a18a6b8ce1f6d64f6a63f256da90432f87
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-24 20:51:39 +0530

    Update with optimal code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,20 @@
 const removeFromArray = function(array, ...args) {
-    let ansArray = [];
-    let present = true;
-    for(let i = 0; i < array.length; i++){
-        present = true;
-        for(let j = 0; j < args.length; j++){
-            if(array[i] === args[j]){
-                present = false;
-            }
-        }
-        if(present === true){
-            ansArray.push(array[i]);
-        }
-    }
-    return ansArray;
+    // let ansArray = [];
+    // let present = true;
+    // for(let i = 0; i < array.length; i++){
+    //     present = true;
+    //     for(let j = 0; j < args.length; j++){
+    //         if(array[i] === args[j]){
+    //             present = false;
+    //         }
+    //     }
+    //     if(present === true){
+    //         ansArray.push(array[i]);
+    //     }
+    // }
+    // return ansArray;
+
+    return array.filter(arg => !args.includes(arg));
 };
 
 // Do not edit below this line
