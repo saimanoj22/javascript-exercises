b21580bde114fe409f7bdb76893172704a3d98f5
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-25 04:15:58 +0530

    Add 10 Solution (All 9 tests passed)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0)return "OOPS";
+    number = parseInt(number); 
+    if(number === 1 || number === 2)return 1;
+    let a = 1, b = 1,c = 0;
+    for(let i = 2; i < number; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
