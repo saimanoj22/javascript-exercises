48bc77db921f76a59d43c0935f45630011dfa42b
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-25 01:46:08 +0530

    Add 08 Solution (All 16 tests passed)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for(let i = 0; i < array.length; i++){
+    total += parseInt(array[i]);
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length === 0)return 0;
+	let product = 1;
+  for(let i = 0; i < array.length; i++){
+    product *= parseInt(array[i]);
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0)return 1;
+	let product = 1;
+  for(let i = 1; i <= a; i++){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
