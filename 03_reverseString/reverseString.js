842d94f76f0b074476f56664a3423af36d553045
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-24 20:24:25 +0530

    Add 03 Solution (All 4 tests passed)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split('')
+    let ansArray = [];
+    let start = strArray.length - 1;
+    for(let i = start; i >= 0; i--){
+        ansArray[start - i] = strArray[i];
+    }
+    let ans = ansArray.join('');
+    return ans;
 };
 
 // Do not edit below this line
