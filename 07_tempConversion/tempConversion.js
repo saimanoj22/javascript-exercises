239292519ba171d8112ff85918e753f812302c41
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-05-25 01:30:18 +0530

    Add 07 Solution (All 6 tests passed)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * (5/9);
+  return parseFloat(temp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (temp * (9 / 5)) + 32;
+  return parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line
