5a40440fd3e215364629785b4ec8a98bfc473bae
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-06-04 15:19:07 +0530

    Add my solution (All 3 tests passed)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    list.forEach(item => {
+        if(!('yearOfDeath' in item)){
+            item['yearOfDeath'] = new Date().getFullYear();
+        }
+    });
+    const array = list.sort(function(lastOne, nextOne){
+        const last = lastOne.yearOfDeath - lastOne.yearOfBirth;
+        const next = nextOne.yearOfDeath - nextOne.yearOfBirth;
+        return last > next ? -1 : 1;
+    });
+    return array[0];
 };
 
 // Do not edit below this line
