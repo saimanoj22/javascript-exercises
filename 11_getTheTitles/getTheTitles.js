6694a17b8459a18e453ce3f639899a373cf20da6
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-06-04 15:37:16 +0530

    Add optimal code

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,5 @@
 const getTheTitles = function(list) {
-    let array = [];
-    for(let i = 0; i < list.length; i++){
-        array.push(list[i].title);
-    }
-    return array;
+    return list.map(element => element.title);
 };
 
 // Do not edit below this line
